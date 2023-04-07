5205083e027caf4702a7611c61f0c3c1d069905f
harin1124 <lolrin1124@gmail.com>
2023-04-07 10:34:01 +0900

    메모 단일 컴포넌트 패널 디자인 수정

--- src/component/MemoPanel.tsx
+++ src/component/MemoPanel.tsx
@@ -5,8 +5,12 @@ type MemoPanel = {title: string; reg_date: string; edit_date: string};
 function MemoPanel(prop:MemoPanel) {
   return (
     <div className={styles.memoPanel}>
-			<p>제목{prop.title}</p>
-      <p>등록일{prop.reg_date}</p>
+      <div className={styles.title}>
+        <p>{prop.title}</p>
+      </div>
+      <div className={styles.date}>
+        <p><span>{prop.reg_date}</span></p>
+      </div>
 		</div>
   );
 }
