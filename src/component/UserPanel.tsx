d900206c927370e1045c3ab005890dc11424b742
harin1124 <lolrin1124@gmail.com>
2023-04-19 17:47:34 +0900

    로그아웃 기능 추가

--- src/component/UserPanel.tsx
+++ src/component/UserPanel.tsx
@@ -1,16 +1,23 @@
 import styles from "../styleModule/userPanel.module.css";
+import { useNavigate } from "react-router-dom";
 
 type UserInfo = {userName: string|null};
 
 function UserPanel(){
+  let navigate = useNavigate();
   let userInfo:UserInfo = {userName: ""};
   userInfo.userName = localStorage.getItem("loginUserId") || null;
+
+  const actionUserLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  }
 
   if(userInfo.userName !== ""){
     return (
       <div className={styles.userPanel}>
         <p className={styles.userPanelName}>{userInfo.userName}</p>
-        <p className={styles.userPanelLogout}>로그아웃</p>
+        <p className={styles.userPanelLogout} onClick={actionUserLogout}>로그아웃</p>
       </div>
     );
   } else {
